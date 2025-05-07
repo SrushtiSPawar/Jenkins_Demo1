69d67e568015c5ce37f7eeb3d544f76b73b3df4d
SrushtiSPawar <pawarsrushti1304@gmail.com>
2025-05-07 21:26:00 +0530

    Update HelloStudent.java

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,6 +2,7 @@ public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello Srushtiii!");
         System.out.println("Welcome to CDAC!");
+         System.out.println("Welcome to DITISS!");
         
     }
 }
